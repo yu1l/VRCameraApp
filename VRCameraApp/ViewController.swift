3283a99f268ff8883e061abd316a63295051d4c3
yuhoshino <yuhoshino@yuhoshinos-MacBook-Pro.local>
2017-08-18 01:04:49 +0900

    指パッチンコマンドを追加しました

--- VRCameraApp/ViewController.swift
+++ VRCameraApp/ViewController.swift
@@ -9,6 +9,18 @@
 import UIKit
 import Photos
 import AVFoundation
+import AudioToolbox
+
+private func AudioQueueInputCallback(
+    _ inUserData: UnsafeMutableRawPointer?,
+    inAQ: AudioQueueRef,
+    inBuffer: AudioQueueBufferRef,
+    inStartTime: UnsafePointer<AudioTimeStamp>,
+    inNumberPacketDescriptions: UInt32,
+    inPacketDescs: UnsafePointer<AudioStreamPacketDescription>?)
+{
+    // Do nothing, because not recoding.
+}
 
 class ViewController: UIViewController {
     
@@ -38,12 +50,78 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var helpMenuButton: UIButton!
     
+    var voiceCommandTimer: Timer!
+    var voiceCommandQueue: AudioQueueRef!
+    var isVoiceCommandFinished = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.currentFilterName = filterNames[currentFilterIndex]
 
         self.setupEyeFrame()
         self.checkCameraAvailability()
+        self.setupVoiceCommand()
+    }
+    
+    private func setupVoiceCommand() {
+        var dataFormat = AudioStreamBasicDescription(mSampleRate: 44100.0,
+                                                     mFormatID: kAudioFormatLinearPCM,
+                                                     mFormatFlags: AudioFormatFlags(kLinearPCMFormatFlagIsBigEndian | kLinearPCMFormatFlagIsSignedInteger | kLinearPCMFormatFlagIsPacked),
+                                                     mBytesPerPacket: 2,
+                                                     mFramesPerPacket: 1,
+                                                     mBytesPerFrame: 2,
+                                                     mChannelsPerFrame: 1,
+                                                     mBitsPerChannel: 16,
+                                                     mReserved: 0)
+        
+        var audioQueue: AudioQueueRef? = nil
+        var error = noErr
+        error = AudioQueueNewInput(&dataFormat,
+                                   AudioQueueInputCallback,
+                                   UnsafeMutableRawPointer(Unmanaged.passUnretained(self).toOpaque()),
+                                   .none,
+                                   .none,
+                                   0,
+                                   &audioQueue)
+        if error == noErr {
+            self.voiceCommandQueue = audioQueue
+        }
+        AudioQueueStart(self.voiceCommandQueue, nil)
+        
+        
+        // Enable level meter
+        var enabledLevelMeter: UInt32 = 1
+        AudioQueueSetProperty(self.voiceCommandQueue, kAudioQueueProperty_EnableLevelMetering, &enabledLevelMeter, UInt32(MemoryLayout<UInt32>.size))
+        
+        self.voiceCommandTimer = Timer.scheduledTimer(timeInterval: 0.05,
+                                          target: self,
+                                          selector: #selector(detectVolume(_:)),
+                                          userInfo: nil,
+                                          repeats: true)
+        self.voiceCommandTimer.fire()
+    }
+    
+    @objc func detectVolume(_ timer: Timer) {
+        // Get level
+        var levelMeter = AudioQueueLevelMeterState()
+        var propertySize = UInt32(MemoryLayout<AudioQueueLevelMeterState>.size)
+        
+        AudioQueueGetProperty(
+            self.voiceCommandQueue,
+            kAudioQueueProperty_CurrentLevelMeterDB,
+            &levelMeter,
+            &propertySize)
+        if levelMeter.mPeakPower >= -10.0 {
+            if !self.isVoiceCommandFinished {
+                self.isVoiceCommandFinished = true
+                self.changeFilter()
+                self.playSound(name: "poyo")
+                let when = DispatchTime.now() + 1.5
+                DispatchQueue.main.asyncAfter(deadline: when, execute: {
+                    self.isVoiceCommandFinished = false
+                })
+            }
+        }
     }
     
     @IBAction func showHelpMenu(_ sender: UIButton) {
@@ -172,11 +250,15 @@ extension ViewController {
     
     @objc func didSingleTap(_ : UITapGestureRecognizer?) {
         // シングルタップ
+        changeFilter()
+        playSound(name: "poyo")
+    }
+    
+    func changeFilter() {
         // フィルターを変更する
         currentFilterIndex += 1
         let index: Int = currentFilterIndex % filterNames.count
         currentFilterName = filterNames[index]
-        playSound(name: "poyo")
     }
     
     @objc func didDoubleTap(_: UITapGestureRecognizer) {
@@ -211,13 +293,16 @@ extension ViewController {
         guard let url = Bundle.main.url(forResource: name, withExtension: "mp3") else { return }
         
         do {
-            try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback)
-            try AVAudioSession.sharedInstance().setActive(true)
+//            try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback)
+//            try AVAudioSession.sharedInstance().setActive(true)
             
             player = try AVAudioPlayer(contentsOf: url)
             guard let player = player else { return }
             if(!player.isPlaying) {
                 player.play()
+                DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 1, execute: { 
+                    self.player?.stop()
+                })
             }
         } catch let error {
             print(error.localizedDescription)
